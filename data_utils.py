5d6675af77a73acad47673b28ca6e40378dbd4ba
nuriaoyaga <nuria.oyaga@gmail.com>
2017-11-23 19:01:03 +0100

    Añadidos comentarios

--- data_utils.py
+++ data_utils.py
@@ -10,6 +10,12 @@ import numpy as np
 
 
 def read_data(file):
+    """
+    Function to read the data according how thy were created (sequence_generator.py)
+
+    :param file: File where the data are saved
+    :return: The data
+    """
 
     f = open(file)
     data = f.readlines()
@@ -19,31 +25,44 @@ def read_data(file):
 
     samples = [np.array(element.split('][')[1].split(' ')[1:-2], dtype=np.float32) for element in data]
 
-    print(samples[0].size)
     return parameters, samples
 
 
 def reshape_data(data):
+    """
+    Function to get the data in a correct shape to use with a Keras network
+
+    :param data: Data to reshape(sample)
+    :return: Data provided, data to predict
+    """
 
     for sample in enumerate(data):
-        points = sample[1]
+        points = sample[1]  # Number the points in a sample (known + to predict)
 
         if sample[0] == 0:
-            dataX = points[0:-1]
-            dataY = points[points.size - 1]
+            dataX = points[0:-1]  # Get known elements
+            dataY = points[points.size - 1]  # Get to predict element
 
         else:
-            dataX = np.vstack([dataX, points[0:-1]])
-            dataY = np.append(dataY, points[points.size - 1])
-
-    print(dataX.shape)
+            dataX = np.vstack([dataX, points[0:-1]])  # Stack all the known elements (each sample)
+            dataY = np.append(dataY, points[points.size - 1])  # Add all to predict element (each shample)
 
     return dataX,dataY
 
 
 def draw_data(fig, data, gap):
+    """
+    Function to draw a sample
+
+    :param fig: Figure where draw
+    :param data: Data to draw
+    :param gap: Gap between know data and data to predict
+    """
+
+    # For know elements
     last_know = len(data[0])
     x = list(range(last_know))
 
     fig.scatter(x, data[0], 5, color='green')
+    # For to predict element
     fig.scatter(last_know - 1 + gap, data[1], 5, color='green')
